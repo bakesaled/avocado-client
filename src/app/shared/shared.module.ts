30ca12f24df5bc2e7155b2b35670b502150fd1d3
Brandon Eleuterio <eleuterio@railroad19.com>
2017-09-08 20:18:05 -0600

    adding material

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MdButtonModule, MdIconModule, MdInputModule, MdSidenavModule, MdToolbarModule } from '@angular/material';
 
+const materialModules = [
+  MdToolbarModule,
+  MdButtonModule,
+  MdIconModule,
+  MdSidenavModule,
+  MdInputModule
+];
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    materialModules
   ],
   exports: [
     CommonModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    materialModules
   ]
 })
 export class SharedModule { }
