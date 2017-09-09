ca2da437103dd736f436760aad0c325c898d8a10
Brandon Eleuterio <eleuterio@railroad19.com>
2017-09-09 16:00:46 -0600

    adding better road segments

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -7,35 +7,38 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 })
 export class BoardComponent implements OnInit {
   private tileSetImage: HTMLImageElement;
+  private roadTileSetImages: Array<HTMLImageElement>;
   private context: CanvasRenderingContext2D;
   private eastWestRoadSegment: RoadSegment;
   private northSouthRoadSegment: RoadSegment;
   private intersectionRoadSegment: RoadSegment;
   private roadSegs: Array<RoadSegment>;
+  private allImagesLoaded: boolean;
+  private imageLoadCount: number = 0;
   
   @ViewChild('board') canvasRef: ElementRef;
 
   constructor() {
     this.eastWestRoadSegment = {
       id: 1,
-      x: 1330,
-      y: 201,
-      width: 1020,
-      height: 716
+      x: 0,
+      y: 0,
+      width: 256,
+      height: 256
     }
     this.northSouthRoadSegment = {
       id: 2,
-      x: 365,
-      y: 660,
-      width: 724,
-      height: 1030
+      x: 0,
+      y: 0,
+      width: 256,
+      height: 256
     }
     this.intersectionRoadSegment = {
       id: 3,
-      x: 1305,
-      y: 1053,
-      width: 1080,
-      height: 1396
+      x: 0,
+      y: 0,
+      width: 256,
+      height: 256
     }
 
     this.roadSegs = [
@@ -55,35 +58,60 @@ export class BoardComponent implements OnInit {
   }
 
   loadTileSetImage() {
-    this.tileSetImage = new Image();
-    this.tileSetImage.src = '../../assets/Road_test.png';
-    this.tileSetImage.onload = () => this.drawBoard();
+    const basePath = '../../assets/roads/';
+
+    const roadEW = new Image();
+    roadEW.src = basePath + 'roadEW.png';
+    roadEW.onload = () => this.drawBoard();
+
+    const roadNS = new Image();
+    roadNS.src = basePath + 'roadNS.png';
+    roadNS.onload = () => this.drawBoard();
+
+    const roadNEWS = new Image();
+    roadNEWS.src = basePath + 'roadNEWS.png';
+    roadNEWS.onload = () => this.drawBoard();
+
+    this.roadTileSetImages = [
+      roadEW,
+      roadNS,
+      roadNEWS
+    ];
+
+    // this.tileSetImage = new Image();
+    // this.tileSetImage.src = '../../assets/Road_test.png';
+    // this.tileSetImage.onload = () => this.drawBoard();
   }
 
   drawBoard() {
+    this.imageLoadCount++;
+    this.allImagesLoaded = this.imageLoadCount === 3;
+
+    if (!this.allImagesLoaded) {
+      return;
+    }
     var map = [
-      [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-      [0,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
-      [0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+      [0,0,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0],
+      [1,1,1,1,1,1,1,3,1,1,1,3,1,1,1,1,1,1,1,1,1,1,1,0],
+      [0,0,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,1,1,3,1,1,1,3,1,1,0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0]
     ];
-    const tileSize = 32;
+    const tileSize = 64;
     const rowTileCount = map.length;
     const colTileCount = map[0].length;
     for (let r = 0; r < rowTileCount; r++) {
       for (let c = 0; c < colTileCount; c++) {
         const tile = map[r][c];
-        // const tileRow = (tile/ image)
         if (tile > 0) {
           const tileSetCell = this.roadSegs[tile - 1]
-          this.context.drawImage(this.tileSetImage, tileSetCell.x, tileSetCell.y, tileSetCell.width, tileSetCell.height, (c * tileSize), (r * tileSize), tileSize, tileSize);
+          this.context.drawImage(this.roadTileSetImages[tile - 1], tileSetCell.x, tileSetCell.y, tileSetCell.width, tileSetCell.height, (c * tileSize), (r * tileSize), tileSize, tileSize);
         }
       }
     }
